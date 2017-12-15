f334c597711381e71be27b9acc5b1f7b2af9b1ab
Kamil Figiela <kamil.figiela@gmail.com>
2017-12-15 18:11:45 +0100

    simple region

--- src/Interpreters.hs
+++ src/Interpreters.hs
@@ -1,30 +1,59 @@
 {-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 
 
 module Interpreters where
-import           Control.Exception (SomeException)
+import           Control.Exception (ArithException (..), SomeException, throwIO)
 import           Domain
 import           Eff
 import           Eff.Exc
+import           Eff.Region
 import           Eff.SafeIO        (SIO, safeIO)
 import           Language.Bar
 import           Language.CashDesk
 import           Language.Kitchen
 
+data Oven = Oven deriving Eq
 
-runKitchen :: (Member SIO r, Member (Exc SomeException) r) => Eff (Kitchen ': r) a -> Eff r a
+instance SafeForRegion Oven '[SIO, Exc SomeException]
+
+type instance ResourceCtor Oven = ()
+
+
+ovenRegion :: forall r a. ( SafeForRegion Oven r, Member SIO r, Member (Exc SomeException) r) => Region Oven r a -> Eff r a
+ovenRegion = handleRegionRelay turnOn turnOff catchSafeIOExcs
+  where
+    turnOn _ = do
+      safeIO $ putStrLn "heating up oven"
+      return (Oven)
+    turnOff _ = do
+      safeIO $ putStrLn "turned oven off"
+
+
+bakePizza :: forall r s. ( s ~ Ancestor 0 r, Member (RegionEff Oven s) r) => Eff r ()
+bakePizza = acquire @Oven () >> return ()
+
+
+runKitchen :: (SafeForRegion Oven r, Member SIO r, Member (Exc SomeException) r) => Eff (Kitchen ': r) a -> Eff r a
 runKitchen = handleRelay pure (\k q -> interpret k >>= q) where
-    interpret :: (Member SIO r, Member (Exc SomeException) r)  => Kitchen x -> Eff r x
-    interpret (OrderPizza pizza) = safeIO (do
-          print pizza
+    interpret :: (SafeForRegion Oven r, Member SIO r, Member (Exc SomeException) r)  => Kitchen x -> Eff r x
+    interpret (OrderPizza pizza) = (do
+          safeIO $ print pizza
+          ovenRegion $ do
+            bakePizza
+            safeIO $ putStrLn $ "baking " ++ (show pizza)
+            _ <- safeIO $ throwIO Overflow
+            safeIO $ putStrLn "this doesn't get run"
           return 12)
 
     interpret (Complain complaint) = safeIO (print complaint)
